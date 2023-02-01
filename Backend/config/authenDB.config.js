0a932595c99a38581c1b0c8cb4948bb90e448635
1909802728861 <cshuhand@gmail.com>
2023-02-01 10:28:37 +0700

    connect to MongoDB Atlas

--- Backend/config/authenDB.config.js
+++ Backend/config/authenDB.config.js
@@ -1,7 +1,9 @@
+// module.exports = {
+//     url: 'mongodb://localhost:27017/authens'
+// }
 module.exports = {
-    url: 'mongodb://localhost:27017/authens'
+    url: 'mongodb+srv://Chonakan:1909802728861@cdticommunitycluster.ivingqb.mongodb.net/?retryWrites=true&w=majority'
 }
-
 // const mongoose = require("mongoose")
 
 // mongoose.connect("mongodb://localhost:27017/authens")
